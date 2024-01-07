67a7bd4974739c63cc95000dbd4c822bc2e63582
trawwa <inekkit@gmail.com>
2024-01-07 23:29:04 +0700

    update

--- crm_service/crm_service/settings.py
+++ crm_service/crm_service/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'crm_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'trawwa',
+        'PASSWORD': 'postgresdb',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
