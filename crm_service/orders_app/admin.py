130a4480db676129c5a37af7e337e9585c524146
trawwa <inekkit@gmail.com>
2024-05-04 15:45:55 +0700

    upd adm

--- crm_service/orders_app/admin.py
+++ crm_service/orders_app/admin.py
@@ -29,4 +29,37 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'my_device_manufacturer', 'my_device_model', 'my_serial_number', 
                     'my_customer', 'order_description', 'created_dt', 'last_updated_dt', 'order_status')
     
-    search_fields = ()
+    search_fields = ('device__customer__customer_name', 'device__id', 'device__serial_number',
+                     'device__analyzer__model', 'device__analyzer__manufacturer')
+    raw_id_fields = ('device', )
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ('customer_name', 'customer_address')
+    list_display = ('id', 'customer_name', 'customer_address', 'customer_city')
+
+
+class DeviceInFieldAdmin(admin.ModelAdmin):
+    def my_customer(self, obj):
+        return obj.customer.customer_name
+    
+    def my_device_model(self, obj):
+        return obj.analyzer.model
+    
+    def my_device_manufacturer(self, obj):
+        return obj.analyzer.manufacturer
+    
+
+    my_customer.short_description = 'Пользователь'
+    my_device_manufacturer.short_description = 'Производитель'
+    my_device_model.short_description = 'Модель'
+
+    search_fields = ('serial_number', )
+    raw_id_fields = ('customer', 'analyzer')
+    list_display = ('id', 'my_device_manufacturer', 'my_device_model', 'serial_number', 'my_customer', 'owner_status')
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(DeviceInField, DeviceInFieldAdmin)
